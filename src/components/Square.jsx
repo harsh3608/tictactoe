934bd21564674fec3358709c9b26bee8fa263f3d
harsh3608 <harsh3608@gmail.com>
2022-02-16 19:45:27 +0530

    styled the entire app

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -4,11 +4,10 @@ function Square({ value, onClick, isWinningSquare }) {
   return (
     <button
       type="button"
-      className="square"
       onClick={onClick}
-      style={{
-        fontWeight: isWinningSquare ? 'bold' : 'normal',
-      }}
+      className={`square ${isWinningSquare ? 'winning' : ''} ${
+        value === 'X' ? 'text-green' : 'text-orange'
+      }`}
     >
       {value}
     </button>
